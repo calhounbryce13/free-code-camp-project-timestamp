fa68a269455065f20c8083ab76be8e0c447be3aa
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-09-24 23:33:51 -0700

    feat: defined the route handler for api/:date endpoint

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+
+app.get('/api/:date', (req, res) => {
+  if(req.params['date']){
+    const rawDate = req.params['date'];
+    console.log(rawDate);
+    let dateObj;
+    try{
+      dateObj = new Date(rawDate);
+    }catch(error){
+      res.status(500).json({"Error": "Invalid date"});
+      return;
+    }
+    res.status(200).json({"unix":dateObj.getTime()});
+    return;
+  }
+  res.status(400).json({"Error": "Invalid request params"});
+  return;
+});
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
